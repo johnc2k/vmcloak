04c639d9f6c297c05826854d3277b2b2a27c06a4
John Cobb <johnc@nobytes.com>
2016-02-27 18:07:39 +0000

    Added: Disable: Windows FW, Updates, and Defender

--- custom/win7.bat
+++ custom/win7.bat
@@ -2,3 +2,5 @@
 netsh int ipv6 isatap set state disabled
 netsh int ipv6 6to4 set state disabled
 netsh interface teredo set state disable
+:: Win7: Disable Windows Firewall
+netsh advfirewall set allprofiles state off
